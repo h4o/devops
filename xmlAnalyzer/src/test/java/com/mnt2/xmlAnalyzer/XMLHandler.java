e3b50ddc3112269a3b48c6fc96f144805b20b298
PierreMassanes <pierre.massanes@gmail.com>
2016-02-25 15:58:35 +0100

    Copy paste qui marche :)

--- xmlAnalyzer/src/test/java/com/mnt2/xmlAnalyzer/XMLHandler.java
+++ xmlAnalyzer/src/test/java/com/mnt2/xmlAnalyzer/XMLHandler.java
@@ -9,21 +9,54 @@ import org.xml.sax.helpers.*;
 /**
  * Classe utilisée pour gérer les évènements émis par SAX lors du traitement de fichiers XML
  */
-public class XMLHandler extends DefaultHandler {
-    private String currentTag = "";
+class TestSAX2Handler extends DefaultHandler
+{
+    private String tagCourant = "";
 
     /**
-     * Action à réaliser lors de la détection d'un nouvel élément
+     * Actions à réaliser lors de la détection d'un nouvel élément.
      */
-    public XMLHandler() {
-        super();
+    public void startElement(String nameSpace, String localName,
+                             String qName, Attributes attr) throws SAXException  {
+        tagCourant = localName;
+        System.out.println("debut tag : " + localName);
     }
 
-    public void startElement(String namespace, String localName, String qName, Attributes attr) throws SAXException {
-
+    /**
+     * Actions à réaliser lors de la détection de la fin d'un élément.
+     */
+    public void endElement(String nameSpace, String localName,
+                           String qName) throws SAXException {
+        tagCourant = "";
+        System.out.println("Fin tag " + localName);
     }
-    public void endElement(String namespace, String localName, String qName) throws SAXException {
-        currentTag = "";
-        System.out.println("End Tag "+localName);
+
+    /**
+     * Actions à réaliser au début du document.
+     */
+    public void startDocument() {
+        System.out.println("Debut du document");
+    }
+
+    /**
+     * Actions à réaliser lors de la fin du document XML.
+     */
+    public void endDocument() {
+        System.out.println("Fin du document");
+    }
+
+    /**
+     * Actions à réaliser sur les données
+     */
+    public void characters(char[] caracteres, int debut,
+                           int longueur) throws SAXException {
+        String donnees = new String(caracteres, debut, longueur);
+
+        if (!tagCourant.equals("")) {
+            if(!Character.isISOControl(caracteres[debut])) {
+                System.out.println("   Element " + tagCourant +",
+                        valeur = *" + donnees + "*");
+            }
+        }
     }
 }
