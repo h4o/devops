c1e286569d62bfe80d9d0cb310145d8c2f36e1a6
PierreMassanes <pierre.massanes@gmail.com>
2016-02-25 15:15:07 +0100

    Ajout de la boucle bash d'exécution du framework

---
+++ run_framework.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH_POM="sample/pom.xml"
+
+PROC_NAMES=( 'UnaryOperatorMutator' )
+
+for NAME in ${PROC_NAMES[@]}
+do
+
+	PROC_PATH="com.mnt2.mutationFramework."${NAME}
+
+	mvn test -f ${PATH_POM} -Dparam_processor=${PROC_PATH}
+
+done
+
