69e0d67050448130fa21e14c62d4147358a5efb0
Trigunale <alexandre.cazala@gmail.com>
2016-02-25 15:40:49 +0100

    XML Analyzer - initial Commit

---
+++ xmlAnalyzer/src/test/java/com/mnt2/xmlAnalyzer/XMLHandler.java
@@ -0,0 +1,11 @@
+package com.mnt2.xmlAnalyzer;
+
+import java.util.*;
+/**
+ * Classe utilisée pour gérer les évènements émis par SAX lors du traitement de fichiers XML
+ */
+public class XMLHandler extends DefaultHandler {
+    public XMLHandler() {
+        super();
+    }
+}
