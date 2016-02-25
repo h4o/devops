e445ad199185924664dfd2b7f86abcf647c42c08
Fabien Vicente <fvicente@polytech.unice.fr>
2016-02-25 16:47:41 +0100

    Refactored for generating reports

---
+++ src/main/java/com/mnt2/mutationFramework/AbstractMutator.java
@@ -0,0 +1,70 @@
+package com.mnt2.mutationFramework;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtUnaryOperator;
+import spoon.reflect.code.UnaryOperatorKind;
+import spoon.reflect.cu.SourcePosition;
+import spoon.reflect.declaration.CtElement;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Fabien VICENTE on 25/02/16.
+ */
+public abstract class AbstractMutator  extends AbstractProcessor<CtElement> {
+    private String report;
+
+    @Override
+    public  void init(){
+        System.out.println("init");
+        report = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
+        report = "<processor name=\""+this.getClass().getName()+"\"";
+        super.init();
+    }
+
+
+    @Override
+    public void processingDone(){
+        report +="</processor>";
+        saveReport();
+        super.processingDone();
+    }
+
+
+
+
+
+
+
+
+    public void addModification(String before, String after, SourcePosition pos){
+        if(before.equals(after))
+            return;
+        try {
+            report += "<modification file=\"" + pos.getFile().getCanonicalPath() +
+                    "\" line=\""+pos.getLine()+"\" column=\""+pos.getColumn()+"\">";
+            report += "<before>"+before+"</before>";
+            report += "<after>"+after+"</after>";
+            report += "</modification>";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    private void saveReport(){
+        try {
+            File f = new File("./target/mutationframework");
+            f.mkdirs();
+            PrintWriter writer = new PrintWriter("./target/mutationframework/"+this.getClass().getName()+".xml",
+                    "UTF-8");
+            writer.println(report);
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/com/mnt2/mutationFramework/UnaryOperatorMutator.java
+++ src/main/java/com/mnt2/mutationFramework/UnaryOperatorMutator.java
@@ -1,14 +1,28 @@
 package com.mnt2.mutationFramework;
 
 import spoon.processing.AbstractProcessor;
+import spoon.processing.ProcessorProperties;
 import spoon.reflect.code.CtUnaryOperator;
 import spoon.reflect.code.UnaryOperatorKind;
+import spoon.reflect.cu.SourcePosition;
 import spoon.reflect.declaration.CtElement;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Fabien VICENTE on 12/02/16.
  */
-public class UnaryOperatorMutator extends AbstractProcessor<CtElement> {
+public class UnaryOperatorMutator extends AbstractMutator {
+
+
+
     @Override
     public boolean isToBeProcessed(CtElement candidate){
         return candidate instanceof CtUnaryOperator;
@@ -21,6 +35,11 @@ public class UnaryOperatorMutator extends AbstractProcessor<CtElement> {
             return;
         }
         CtUnaryOperator op = (CtUnaryOperator) candidate;
+        addModification(op.getKind().name(),UnaryOperatorKind.POSTDEC.name(),op.getPosition());
         op.setKind(UnaryOperatorKind.POSTDEC);
+
+
     }
+
+
 }
