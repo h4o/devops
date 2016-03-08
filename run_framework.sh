b626c6322cacf0897cc32b8ab582ef5cb39fff7f
Trigunale <alexandre.cazala@gmail.com>
2016-03-08 18:07:18 +0100

    IL EST VIVANT !

--- run_framework.sh
+++ run_framework.sh
@@ -3,14 +3,15 @@
 PATH_POM="sample/pom.xml"
 
 PROC_NAMES=( 'EmptyProcessor' 'UnaryOperatorMutator' 'LogicalOperatorMutator'  )
-
+declare -i cpt=0
 for NAME in ${PROC_NAMES[@]}
 do
 
 	PROC_PATH="com.mnt2.mutationFramework."${NAME}
-
 	mvn test -f ${PATH_POM} -Dparam_processor=${PROC_PATH}
-
+	mv ./sample/target/mutationframework/com.mnt2.mutationFramework.* ./sample/output/processor/MUT-$((cpt)).xml
+    mv ./sample/target/surefire-reports/TEST-* ./sample/output/tests/TEST-$((cpt)).xml
+    cpt=$((cpt + 1))
 done
 
 mvn clean compile -f "xmlAnalyzer/pom.xml" assembly:single
--- xmlAnalyzer/src/main/java/com/mnt2/xmlAnalyzer/XMLAnalyzer.java
+++ xmlAnalyzer/src/main/java/com/mnt2/xmlAnalyzer/XMLAnalyzer.java
@@ -5,10 +5,8 @@ public class XMLAnalyzer {
         IParseTest parser = new XMLParser();
         String DIR_PATH = System.getProperty("user.dir");
         String HTML_PATH = DIR_PATH+"/HTMLGenerated/";
-        /*IGenerator generator = new GeneratorHTML(HTML_PATH);
-        parser.parse(DIR_PATH+"/sample/target/surefire-reports/TEST-com.mnt2.sample.TestClassTest.xml");
-        generator.generateMutant(parser.parse(DIR_PATH+"/sample/target/surefire-reports/TEST-com.mnt2.sample.TestClassTest.xml"),
-             );
-*/
+        IGenerator generator = new GeneratorHTML(parser.parse(DIR_PATH+"/sample/TEST-0.xml"),HTML_PATH);
+        generator.generateMutant(parser.parse(DIR_PATH+"/sample/TEST-1.xml"));
+
     }
 }
