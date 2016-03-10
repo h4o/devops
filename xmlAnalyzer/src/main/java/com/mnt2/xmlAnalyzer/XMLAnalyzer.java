7ef42c71e6e847b80328077c318a06a004b241ba
Trigunale <alexandre.cazala@gmail.com>
2016-03-10 10:21:49 +0100

    L'analyzer marche sur tout le contenu d'un dossier et non plus seulement sur les fichiers que l'on a spécifié

--- xmlAnalyzer/src/main/java/com/mnt2/xmlAnalyzer/XMLAnalyzer.java
+++ xmlAnalyzer/src/main/java/com/mnt2/xmlAnalyzer/XMLAnalyzer.java
@@ -1,5 +1,9 @@
 package com.mnt2.xmlAnalyzer;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class XMLAnalyzer {
     public static void main(String[] args) {
 
@@ -7,10 +11,22 @@ public class XMLAnalyzer {
         String HTML_PATH = DIR_PATH+"/HTMLGenerated/";
 
         IParseTest parser = new XMLParser();
+
+        // Get content of a folder and store each file name
+        File f = new File(DIR_PATH+"/sample/output/tests/");
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list()));
+
+        // On parse le rapport du code sans mutation (mutateur vide appliqué)
         IGenerator generator = new GeneratorHTML(parser.parse(DIR_PATH+"/sample/output/tests/TEST-0.xml"),HTML_PATH);
-        generator.generateMutant(parser.parse(DIR_PATH+"/sample/output/tests/TEST-1.xml"));
-        generator.generateMutant(parser.parse(DIR_PATH+"/sample/output/tests/TEST-0.xml"));
-        generator.generateMutant(parser.parse(DIR_PATH+"/sample/output/tests/TEST-2.xml"));
+        // On enlève le fichier de la liste
+        names.remove("TEST-0.xml");
+
+        // Pour chaque fichier on le parse et on prépare le rapport HTML
+        for (String testReportFile : names) {
+            generator.generateMutant(parser.parse(DIR_PATH+"/sample/output/tests/"+testReportFile));
+        }
+
+        // On finalise le rapport
         generator.generateReport();
     }
 }
